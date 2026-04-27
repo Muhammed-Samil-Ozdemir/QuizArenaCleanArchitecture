24b82be8b7a001fce5167b6d2c06dc83bc22eae6
Muhammed Şamil Özdemir <msamil6042@gmail.com>
2026-04-27 17:16:05 +0300

    Repositories DI yapıldı

--- QuizArena.Persistance/PersistanceRegistrar.cs
+++ QuizArena.Persistance/PersistanceRegistrar.cs
@@ -1,7 +1,21 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using QuizArena.Domain.QuestionOptions;
+using QuizArena.Domain.Questions;
+using QuizArena.Domain.RoomParticipants;
+using QuizArena.Domain.Rooms;
+using QuizArena.Domain.UnitOfWorks;
+using QuizArena.Domain.UserAnswers;
+using QuizArena.Domain.Users;
 using QuizArena.Persistance.Context;
+using QuizArena.Persistance.QuestionOptions;
+using QuizArena.Persistance.Questions;
+using QuizArena.Persistance.RoomParticipants;
+using QuizArena.Persistance.Rooms;
+using QuizArena.Persistance.UnitOfWorks;
+using QuizArena.Persistance.UserAnswers;
+using QuizArena.Persistance.Users;
 
 namespace QuizArena.Persistance;
 
@@ -11,6 +25,14 @@ public static class PersistanceRegistrar
     {      
         services.AddDbContext<AppDbContext>(options =>
             options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IRoomRepository, RoomRepository>();
+        services.AddScoped<IQuestionRepository, QuestionRepository>();
+        services.AddScoped<IQuestionOptionRepository, QuestionOptionRepository>();
+        services.AddScoped<IRoomParticipantRepository, RoomParticipantRepository>();
+        services.AddScoped<IUserAnswerRepository, UserAnswerRepository>();
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
         
         return services;
     }
